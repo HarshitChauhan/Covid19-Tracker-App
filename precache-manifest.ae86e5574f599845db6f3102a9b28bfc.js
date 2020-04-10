e445b4613b804a89e49ca23216151dc17948a6a9
HarshitChauhan <harshitkumar2503@gmail.com>
2020-04-10 17:03:05 +0530

    Updates

---
+++ precache-manifest.ae86e5574f599845db6f3102a9b28bfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "638f5251b3b9d74155e7f115a089e05a",
+    "url": "/Covid19-Tracker-App/index.html"
+  },
+  {
+    "revision": "9a3bb33bf229c9a48442",
+    "url": "/Covid19-Tracker-App/static/css/main.44b9cf9b.chunk.css"
+  },
+  {
+    "revision": "f993802a7b3f3f8880db",
+    "url": "/Covid19-Tracker-App/static/js/2.bfae1350.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19-Tracker-App/static/js/2.bfae1350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a3bb33bf229c9a48442",
+    "url": "/Covid19-Tracker-App/static/js/main.12908826.chunk.js"
+  },
+  {
+    "revision": "ce3978072c0536d2517f",
+    "url": "/Covid19-Tracker-App/static/js/runtime-main.9bed23cb.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19-Tracker-App/static/media/image.d7265326.png"
+  }
+]);
